1959f1a669f589bc716b6421afc3c371768ae664
keyvan <keyvan.razavi20@gmail.com>
2024-06-15 03:00:14 +0330

    start to implement new changes on frontend

--- frontend/src/components/admin/AdminPage.jsx
+++ frontend/src/components/admin/AdminPage.jsx
@@ -1,13 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AdminPage.css";
 
 const AdminPage = () => {
+  const [title, setTitle] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [dateAndTime, setDateAndTime] = useState("");
+  const [room, setroom] = useState("");
+
+  const addMovie = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8000/api/movies/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          imageUrl,
+          dateAndTime,
+          room,
+        }),
+      });
+      const responseData = await response.json();
+      console.log(responseData);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="admin-container">
       <div className="subMenu">
-        <div>Add Movie</div>
-        <div>Remove Movie</div>
-        <div>Add Movie</div>
+        <div className="add-form">
+          <h3 className="title">Add Movie:</h3>
+          <form onSubmit={(e) => addMovie(e)} className="form">
+            <div className="fields">
+              <label htmlFor="title">Title:</label>
+              <input type="text" id="title" onChange={(e) => setTitle(e)} />
+            </div>
+            <div className="fields">
+              <label htmlFor="poster">Movie poster:</label>
+              <input type="text" id="poster" onChange={(e) => setImageUrl(e)} />
+            </div>
+            <div className="fields">
+              <label htmlFor="date">Date and time:</label>
+              <input
+                type="date"
+                id="date"
+                onChange={(e) => setDateAndTime(e)}
+              />
+            </div>
+            <div className="fields">
+              <label htmlFor="room">Room:</label>
+              <input type="text" id="room" onChange={(e) => setroom(e)} />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <h3 > Movies on Screen </h3>
+        <div className="movies-container">
+          {/* {movies.map((m) => {
+            return (
+              <div key={m.id} className="movie-card">
+                <img src={m.imageUrl} alt="" />
+                <div className="movie-info">
+                  <div>
+                    <h1 style={{ marginBottom: 0 }}>Title: </h1>
+                    <p>{m.title}</p>
+                  </div>
+                  <div>
+                    <h1 style={{ marginBottom: 0 }}>Date And Time: </h1>
+                    <p>{m.dateAndTime}</p>
+                  </div>
+                  <div>
+                    <h1 style={{ marginBottom: 0 }}>Room: </h1>
+                    <p>{m.room}</p>
+                  </div>
+                  <button>Edit</button>
+                </div>
+              </div>
+            );
+          })} */}
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/user/UserPage.jsx
+++ frontend/src/components/user/UserPage.jsx
@@ -1,168 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./UserPage.css";
+import Movies from "../movies/Movies";
 
 const UserPage = () => {
-  const movies = [
-    {
-      id: "01",
-      title: "One flew over the cuckoo's nest",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
-      dateAndTime: "12am - Sep 01 2024 ",
-      room: "blue",
-    },
-    {
-      id: "02",
-      title: "Shining",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+  const [movie, setMovie] = useState();
+  useEffect(() => {
+    const sendRequest = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/api/movies", {
+          method: "GET",
+          mode: "no-cors",
+        });
+        const responseData = await response.json();
+        setMovie(responseData.movies[0]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    sendRequest();
+  }, []);
 
-      dateAndTime: "2pm - Sep 01 2024 ",
-      room: "red",
-    },
-    {
-      id: "03",
-      title: "Sacrifice",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BNTczNDE2YTItNjkwMC00YTNhLTkwMjAtOTIxYmIyZmI1MWUyXkEyXkFqcGdeQXVyMjgyNjk3MzE@._V1_.jpg",
-      dateAndTime: "4:30pm - Sep 01 2024 ",
-      room: "green",
-    },
-    {
-      id: "04",
-      title: "Wild strawberries",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BMTU1MTc3ODMwMl5BMl5BanBnXkFtZTYwMzA2MzI5._V1_.jpg",
-      dateAndTime: "7:00pm - Sep 01 2024 ",
-      room: "green",
-    },
-    {
-      id: "01",
-      title: "One flew over the cuckoo's nest",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
-      dateAndTime: "12am - Sep 01 2024 ",
-      room: "blue",
-    },
-    {
-      id: "02",
-      title: "Shining",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
-
-      dateAndTime: "2pm - Sep 01 2024 ",
-      room: "red",
-    },
-    {
-      id: "03",
-      title: "Sacrifice",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BNTczNDE2YTItNjkwMC00YTNhLTkwMjAtOTIxYmIyZmI1MWUyXkEyXkFqcGdeQXVyMjgyNjk3MzE@._V1_.jpg",
-      dateAndTime: "4:30pm - Sep 01 2024 ",
-      room: "green",
-    },
-    {
-      id: "04",
-      title: "Wild strawberries",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BMTU1MTc3ODMwMl5BMl5BanBnXkFtZTYwMzA2MzI5._V1_.jpg",
-      dateAndTime: "7:00pm - Sep 01 2024 ",
-      room: "green",
-    },
-    {
-      id: "01",
-      title: "One flew over the cuckoo's nest",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
-      dateAndTime: "12am - Sep 01 2024 ",
-      room: "blue",
-    },
-    {
-      id: "02",
-      title: "Shining",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
-
-      dateAndTime: "2pm - Sep 01 2024 ",
-      room: "red",
-    },
-    {
-      id: "03",
-      title: "Sacrifice",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BNTczNDE2YTItNjkwMC00YTNhLTkwMjAtOTIxYmIyZmI1MWUyXkEyXkFqcGdeQXVyMjgyNjk3MzE@._V1_.jpg",
-      dateAndTime: "4:30pm - Sep 01 2024 ",
-      room: "green",
-    },
-    {
-      id: "04",
-      title: "Wild strawberries",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BMTU1MTc3ODMwMl5BMl5BanBnXkFtZTYwMzA2MzI5._V1_.jpg",
-      dateAndTime: "7:00pm - Sep 01 2024 ",
-      room: "green",
-    },
-    {
-      id: "01",
-      title: "One flew over the cuckoo's nest",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
-      dateAndTime: "12am - Sep 01 2024 ",
-      room: "blue",
-    },
-    {
-      id: "02",
-      title: "Shining",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
-
-      dateAndTime: "2pm - Sep 01 2024 ",
-      room: "red",
-    },
-    {
-      id: "03",
-      title: "Sacrifice",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BNTczNDE2YTItNjkwMC00YTNhLTkwMjAtOTIxYmIyZmI1MWUyXkEyXkFqcGdeQXVyMjgyNjk3MzE@._V1_.jpg",
-      dateAndTime: "4:30pm - Sep 01 2024 ",
-      room: "green",
-    },
-    {
-      id: "04",
-      title: "Wild strawberries",
-      imageUrl:
-        "https://m.media-amazon.com/images/M/MV5BMTU1MTc3ODMwMl5BMl5BanBnXkFtZTYwMzA2MzI5._V1_.jpg",
-      dateAndTime: "7:00pm - Sep 01 2024 ",
-      room: "green",
-    },
-  ];
   return (
     <div className="user-container">
       <div className="subMenu">
-        <h3>Movies on Screen</h3>
+        <h3>Movies on Screen </h3>
         <div className="movies-container">
-          {movies.map((m) => {
-            return (
-              <div key={m.id} className="movie-card">
-                <img src={m.imageUrl} alt="" />
-                <div className="movie-info">
-                  <div>
-                    <h1 style={{ marginBottom: 0 }}>Title: </h1>
-                    <p>{m.title}</p>
-                  </div>
-                  <div>
-                    <h1 style={{ marginBottom: 0 }}>Date And Time: </h1>
-                    <p>{m.dateAndTime}</p>
-                  </div>
-                  <div>
-                    <h1 style={{ marginBottom: 0 }}>Room: </h1>
-                    <p>{m.room}</p>
-                  </div>
-                  <button>Book</button>
-                </div>
-              </div>
-            );
-          })}
+          <Movies movie={movie} />
         </div>
       </div>
     </div>
